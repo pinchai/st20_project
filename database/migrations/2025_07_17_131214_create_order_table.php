1a85258cc67ae9e990c367dec33b4c3110915f81
Pinchai <>
2025-07-17 20:22:46 +0700

    add migrate and layout

---
+++ database/migrations/2025_07_17_131214_create_order_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->dateTime('datetime');
+            $table->unsignedBigInteger('payment_method_id');
+            $table->enum('status', ['pending', 'packaging', 'on_delivery', 'complete'])
+                ->default('pending');
+            $table->string('remark')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order');
+    }
+};
